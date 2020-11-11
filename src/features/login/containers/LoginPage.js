56151b8fcf5fa7b0a57df8e51b1d15646d024fc7
Kurniawan Yusuf <kurniawanyusuf.w@gmail.com>
2020-11-11 10:51:17 +0700

    add form validation

--- src/features/login/containers/LoginPage.js
+++ src/features/login/containers/LoginPage.js
@@ -3,17 +3,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { getUserAsync } from "../login.api";
-import "./login.css";
 
 const LoginPage = () => {
+  const [validated, setValidated] = useState(false);
   const dispatch = useDispatch();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const userState = useSelector((state) => state.login.users[0]);
 
   const submitHandler = (event) => {
-    dispatch(getUserAsync(username, password));
-    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      dispatch(getUserAsync(username, password));
+      event.preventDefault();
+    }
+
+    setValidated(true);
   };
 
   if (userState !== undefined) {
@@ -29,31 +37,35 @@ const LoginPage = () => {
             marginTop: "4em",
             padding: "2em",
             backgroundColor: "#efefef",
+            borderTop: "5px solid #4285f4",
+            borderRadius: "5px",
           }}
         >
           <h4 className="text-center mb-4">Login</h4>
           <Row className="justify-content-md-center">
             <Col md={8}>
-              <Form onSubmit={submitHandler}>
+              <Form noValidate validated={validated} onSubmit={submitHandler}>
                 <Form.Group controlId="formBasicEmail">
                   <Form.Label className="text-left">
                     Username or Email address
                   </Form.Label>
                   <Form.Control
+                    required
                     type="text"
-                    placeholder="Enter email"
+                    placeholder="Enter email or username"
                     value={username}
                     onChange={(event) => {
                       setUsername(event.target.value);
                     }}
                   />
                   <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
+                    We'll never share your email or username with anyone else.
                   </Form.Text>
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
                   <Form.Control
+                    required
                     type="password"
                     placeholder="Password"
                     value={password}
@@ -62,8 +74,8 @@ const LoginPage = () => {
                     }}
                   />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                  Submit
+                <Button variant="primary" type="submit" className="float-right">
+                  Login
                 </Button>
               </Form>
             </Col>
